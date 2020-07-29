2eb61df64e04543756ac19459236857760b78645
marcin.tyborowski@billennium.com <mtyborowski09@gmail.com>
2020-07-29 21:16:05 +0200

    add filtering with debouncetime and subject

--- src/app/main/training/modules/training-details/components/add-training-exercise-modal/add-training-exercise-modal.component.ts
+++ src/app/main/training/modules/training-details/components/add-training-exercise-modal/add-training-exercise-modal.component.ts
@@ -1,16 +1,17 @@
+import { switchMap, debounceTime } from 'rxjs/operators';
 import { ExerciseModel } from './../../../../../exercises/models/exercise.model';
 import { ExerciseClient } from '../../../../../../services/api/api.service';
 import { delay } from 'rxjs/internal/operators';
 import { getExercises } from '../../../../../exercises/store/exercise.selectors';
 import { ExerciseState } from '../../../../../exercises/store/exercise.state';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject, EMPTY, Subject, combineLatest } from 'rxjs';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Validators, FormBuilder } from '@angular/forms';
 import { FormControl } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { map, startWith } from 'rxjs/operators';
+import { map, startWith, mergeMap } from 'rxjs/operators';
 import { Constants } from 'src/app/constants/constants';
 
 @Component({
@@ -20,8 +21,13 @@ import { Constants } from 'src/app/constants/constants';
 })
 export class AddTrainingExerciseModalComponent implements OnInit {
   filteredExercises$: Observable<ExerciseModel[]>;
+  // filteredExercises: ExerciseModel[];
+
   selectedExercise: ExerciseModel;
   bodyParts = Constants.bodyParts;
+
+  searchTerm = new Subject<string>();
+  searchTermChanged$ = this.searchTerm.asObservable();
 
   addTrainingExerciseForm = this.formBuilder.group({
     exercise: ['', Validators.required],
@@ -35,7 +41,7 @@ export class AddTrainingExerciseModalComponent implements OnInit {
     breakTime: [60, Validators.required],
     suggestion: [''],
     alternativeExercise: [''],
-    weight: [0]
+    weight: [0],
   });
 
   constructor(
@@ -43,17 +49,40 @@ export class AddTrainingExerciseModalComponent implements OnInit {
     private formBuilder: FormBuilder,
     private exerciseService: ExerciseClient
   ) {
+    // WORKING
+    // with subject/behaviorSubject
     this.addTrainingExerciseForm
       .get('exercise')
-      .valueChanges.subscribe(
-        (value) => (this.filteredExercises$ = this.filterExercises(value))
-      );
+      .valueChanges.pipe(debounceTime(1000))
+      .subscribe((term) => this.searchTerm.next(term));
 
     this.addTrainingExerciseForm
       .get('alternativeExercise')
-      .valueChanges.subscribe(
-        (value) => (this.filteredExercises$ = this.filterExercises(value))
-      );
+      .valueChanges.pipe(debounceTime(1000))
+      .subscribe((term) => this.searchTerm.next(term));
+
+    this.filteredExercises$ = this.searchTermChanged$.pipe(
+      switchMap((value) => this.filterExercises(value))
+    );
+
+    // WORKING
+    // subscribe with pipe in html
+    // this.filteredExercises$ = this.addTrainingExerciseForm
+    //   .get('exercise')
+    //   .valueChanges.pipe(
+    //     debounceTime(1000),
+    //     switchMap((value) => this.filterExercises(value))
+    //   );
+
+    // WORKING
+    // subscribe here and assign to model
+    // this.addTrainingExerciseForm
+    //   .get('exercise')
+    //   .valueChanges.pipe(
+    //     debounceTime(1000),
+    //     switchMap((value) => this.filterExercises(value))
+    //   )
+    //   .subscribe((result) => (this.filteredExercises = result));
   }
 
   ngOnInit(): void {}
@@ -74,6 +103,7 @@ export class AddTrainingExerciseModalComponent implements OnInit {
     if (typeof value === 'string') {
       return this.exerciseService.getExercisesByTerm(value.toLowerCase());
     }
+    return EMPTY;
   }
 
   updateSelectedExercise($event) {
